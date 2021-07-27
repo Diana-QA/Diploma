d9c7d53d40794c71d3c7109a1f1bb18f2370e567
Diana Krapivina <mr.ikoma@mail.ru>
2021-07-27 13:58:05 +0300

    Add CreditCardPaymentTest

--- src/test/java/ru/netology/web/test/CreditCardPaymentTest.java
+++ src/test/java/ru/netology/web/test/CreditCardPaymentTest.java
@@ -1,2 +1,27 @@
-package ru.netology.web.test;public class CreditCardPaymentTest {
+package ru.netology.web.test;
+
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class CreditCardPaymentTest {
+
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:8080");
+    }
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
 }
